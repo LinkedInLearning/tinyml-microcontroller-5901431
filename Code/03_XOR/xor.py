e8622c83a7a5ca7773bc4d6f1a9a2306ab95ab50
Lars Gregori <138015+choas@users.noreply.github.com>
2024-05-08 10:40:29 +0200

    remove recording code
    this code was used to have some movement during the recording

--- Code/03_XOR/xor.py
+++ Code/03_XOR/xor.py
@@ -19,25 +19,6 @@ def input_callback(microlite_interpreter):
     input_tensor.setValue(0, button_pin1.value())
     input_tensor.setValue(1, button_pin2.value())
 
-    global c1, c2, p1, p2
-    c1 += 1
-    c2 += 1
-    if c1 > 2000:
-        c1 = 0
-        if p1 == 0:
-            p1 = 1
-        else:
-            p1 = 0
-    if c2 > 800:
-        c2 = 0
-        if p2 == 0:
-            p2 = 1
-        else:
-            p2 = 0
-
-    input_tensor.setValue(0, p1)
-    input_tensor.setValue(1, p2)
-
 # Define the output callback function
 def output_callback(microlite_interpreter):
     global prediction_result
@@ -47,17 +28,11 @@ def output_callback(microlite_interpreter):
 # Create the Microlite interpreter
 interpreter = microlite.interpreter(xor_model_data, 2048, input_callback, output_callback)
 
-c1 = 0
-c2 = 0
-p1 = 0
-p2 = 0
-
 while True:
     # Invoke the interpreter
     interpreter.invoke()
 
-    # print(f"Button1: {button_pin1.value()} Button2: {button_pin2.value()} Output: {prediction_result}")
-    print(f"Button1: {p1} Button2: {p2} Output: {prediction_result}")
+    print(f"Button1: {button_pin1.value()} Button2: {button_pin2.value()} Output: {prediction_result}")
 
     # If the prediction result is greater than 0.5, turn on the LED
     if prediction_result > 0.5:
